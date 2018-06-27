edae934a7b2d9c233b331a10163df1cc371f8888
standen <baileyb7@yahoo.com>
2018-06-27 15:22:09 -0400

    revised version

--- src/Labtwo.java
+++ src/Labtwo.java
@@ -1,43 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.Scanner;
 
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+import java.text.SimpleDateFormat;
 
 public class Labtwo {
-    public static void main(String[] args) throws IOException {
+    
+	public static void main(String[] args) throws ParseException,
+	java.text.ParseException {
+			  
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+	
+    	double yearDelta, monthDelta, dayDelta;
     	
-    	long days, yearDelta, monthDelta, dayDelta;
-    	
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd mm yyyy");
-        //final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Scanner scnr = new Scanner (new InputStreamReader(System.in));
-        //System.out.print("test");
         
         //Get first date
-        System.out.println("Enter First Date ");
-        //final String firstInput = reader.readLine(); - from example
-        final String firstInput = scnr.nextLine();
+        System.out.println("Enter First Date (DD/MM/YYYY) ");
+        // System.out.println("");
+        Date firstDate = sdf.parse(scnr.nextLine());
         
         //Get second date
-        System.out.println("Enter Second Date ");
-        //final String secondInput = reader.readLine(); - from example
-        final String secondInput = scnr.nextLine();
+        System.out.println("Enter Second Date (DD/MM/YYYY) ");
+        Date secondDate = sdf.parse(scnr.nextLine());
         
-        final LocalDate firstDate = LocalDate.parse(firstInput, formatter);
-        final LocalDate secondDate = LocalDate.parse(secondInput, formatter);
-        days = ChronoUnit.DAYS.between(firstDate, secondDate);
+        long diffInMillies = Math.abs(secondDate.getTime() - firstDate.getTime());
+	    long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+	    
+        yearDelta = (diff / 365.25);
+        long years = (long) yearDelta; 
+        dayDelta = diff - (years * 365.25);
+        monthDelta = dayDelta / 30.4375;
+        long months = (long) monthDelta;
+        dayDelta = dayDelta - (months * 30.4375);
+        long days = (long) dayDelta;
         
-        yearDelta = days / 365;
-        dayDelta = days - (yearDelta * 365) + (yearDelta / 4);
-        monthDelta = days / 30;
-        dayDelta = dayDelta - (monthDelta * 30);
-        
-        System.out.println("It is " + yearDelta + "years " + 
-        					monthDelta + "months and " + dayDelta + 
-        					"days between dates.");
-    }
+        System.out.println("It is " + years + " years " + 
+        					months + " months and " + days + 
+        					" days between dates.");
+	}
 }
